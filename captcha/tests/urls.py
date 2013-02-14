84896d25a1e850671f08dac2ef956f0bf641f6ab
Alexey Boriskin <sun.void@gmail.com>
2013-02-14 14:19:02 +0400

    Update for django1.5 and master

--- captcha/tests/urls.py
+++ captcha/tests/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import url, patterns, include
+except ImportError:
+    from django.conf.urls.defaults import url, patterns, include
+
 urlpatterns = patterns('',
     url(r'test/$','captcha.tests.views.test',name='captcha-test'),
     url(r'test2/$','captcha.tests.views.test_custom_error_message',name='captcha-test-custom-error-message'),
--- captcha/urls.py
+++ captcha/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('captcha.views',
     url(r'image/(?P<key>\w+)/$', 'captcha_image', name='captcha-image'),
