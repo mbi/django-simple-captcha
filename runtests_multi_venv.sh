752cd51ee74438711b226db0303020d161ce1e89
Marco Bonetti <mbonetti@gmail.com>
2013-03-06 17:45:29 +0100

    freeze an older commit of Pillow because the current HEAD is borked after 170bb0a78fbfdec7aefcac986462330807760632

--- runtests_multi_venv.sh
+++ runtests_multi_venv.sh
@@ -5,6 +5,40 @@
 
 export CAPTCHA_FLITE_PATH=`which flite`
 
+
+if [ ! -d venv_13 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_13
+    . venv_13/bin/activate
+    pip install Django==1.3 PIL coverage six
+    deactivate
+fi
+if [ ! -d venv_14 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_14
+    . venv_14/bin/activate
+    pip install Django==1.4 PIL  coverage six
+    deactivate
+fi
+if [ ! -d venv_15 ]
+then
+    virtualenv --no-site-packages --distribute --python=python2 venv_15
+    . venv_15/bin/activate
+    pip install Django==1.5 PIL coverage six
+    deactivate
+fi
+if [ ! -d venv_15_p3 ]
+then
+    virtualenv --no-site-packages --distribute --python=python3 venv_15_p3
+    . venv_15_p3/bin/activate
+    pip install Django==1.5 coverage six
+    pip install -e git+https://github.com/python-imaging/Pillow.git@170bb0a78fbfdec7aefcac986462330807760632#egg=pillow-p3-devel
+    deactivate
+fi
+
+
+
+
 . venv_13/bin/activate
 cd testproject
 echo 'Django' `python manage.py --version`
