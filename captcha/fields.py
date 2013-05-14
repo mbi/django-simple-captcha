3e73709af6de5bd6828ed7e9989187747af416b9
Marco Bonetti <mbonetti@gmail.com>
2013-05-14 13:57:01 +0200

    always test for valid output_format and that urlconf is properly loaded

--- captcha/fields.py
+++ captcha/fields.py
@@ -30,11 +30,10 @@ class BaseCaptchaTextInput(MultiWidget):
         Fetches a new CaptchaStore
         This has to be called inside render
         """
-        if django_settings.DEBUG:
-            try:
-                reverse('captcha-image', args=('dummy',))
-            except NoReverseMatch:
-                raise ImproperlyConfigured('Make sure you\'ve included captcha.urls as explained in the INSTALLATION section on http://readthedocs.org/docs/django-simple-captcha/en/latest/usage.html#installation')
+        try:
+            reverse('captcha-image', args=('dummy',))
+        except NoReverseMatch:
+            raise ImproperlyConfigured('Make sure you\'ve included captcha.urls as explained in the INSTALLATION section on http://readthedocs.org/docs/django-simple-captcha/en/latest/usage.html#installation')
 
         key = CaptchaStore.generate_key()
 
@@ -64,13 +63,13 @@ class CaptchaTextInput(BaseCaptchaTextInput):
     def __init__(self, attrs=None, **kwargs):
         self._args = kwargs
         self._args['output_format'] = self._args.get('output_format') or settings.CAPTCHA_OUTPUT_FORMAT
-        if django_settings.DEBUG:
-            for key in ('image', 'hidden_field', 'text_field'):
-                if '%%(%s)s' % key not in self._args['output_format']:
-                    raise ImproperlyConfigured('All of %s must be present in your CAPTCHA_OUTPUT_FORMAT setting. Could not find %s' % (
-                        ', '.join(['%%(%s)s' % k for k in ('image', 'hidden_field', 'text_field')]),
-                        '%%(%s)s' % key
-                    ))
+
+        for key in ('image', 'hidden_field', 'text_field'):
+            if '%%(%s)s' % key not in self._args['output_format']:
+                raise ImproperlyConfigured('All of %s must be present in your CAPTCHA_OUTPUT_FORMAT setting. Could not find %s' % (
+                    ', '.join(['%%(%s)s' % k for k in ('image', 'hidden_field', 'text_field')]),
+                    '%%(%s)s' % key
+                ))
         super(CaptchaTextInput, self).__init__(attrs)
 
     def format_output(self, rendered_widgets):
