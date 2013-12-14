fd86b0aef235680c261f2241664b555b0f336c79
Tomasz <tomasz@mikran.pl>
2013-12-14 22:48:22 +0100

    Update models.py
    fixed for 1.5

--- captcha/models.py
+++ captcha/models.py
@@ -5,7 +5,7 @@ import datetime
 import random
 import time
 import unicodedata
-import six
+import django.utils.six
 
 # Heavily based on session key generation in Django
 # Use the system (hardware-based) random number generator if it exists.
