8591e265e269134c5bf5f8933d0d203895b69590
Marco Bonetti <mbonetti@gmail.com>
2013-12-27 13:32:52 +0100

    version dump, dropping support for Django 1.3

--- runtests_multi_venv.sh
+++ runtests_multi_venv.sh
@@ -6,13 +6,6 @@
 export CAPTCHA_FLITE_PATH=`which flite`
 
 
-if [ ! -d .venv_13 ]
-then
-    virtualenv --no-site-packages --distribute --python=python2 .venv_13
-    source .venv_13/bin/activate
-    pip install Django==1.3 Pillow==2.0.0 coverage six
-    deactivate
-fi
 if [ ! -d .venv_14 ]
 then
     virtualenv --no-site-packages --distribute --python=python2 .venv_14
@@ -38,7 +31,7 @@ if [ ! -d .venv_16 ]
 then
     virtualenv --no-site-packages --distribute --python=python2 .venv_16
     source .venv_16/bin/activate
-    pip install Django==1.6.0
+    pip install Django\>=1.6.0
     pip install Pillow==2.0.0 coverage six
     deactivate
 fi
@@ -46,19 +39,11 @@ if [ ! -d .venv_16_p3 ]
 then
     virtualenv --no-site-packages --distribute --python=python3 .venv_16_p3
     source .venv_16_p3/bin/activate
-    pip install Django==1.6.0
+    pip install Django\>=1.6.0
     pip install Pillow==2.0.0 coverage six
     deactivate
 fi
 
-
-source .venv_13/bin/activate
-cd testproject
-echo 'Django' `python manage.py --version`
-python --version
-python manage.py test captcha
-cd ..
-deactivate
 
 source .venv_14/bin/activate
 cd testproject
