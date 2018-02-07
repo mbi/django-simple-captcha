2c2c9f2c7f3a2b812dc1353a64a4907e746a0f9d
Vlastimil Zíma <vlastimil.zima@nic.cz>
2018-02-07 12:42:08 +0100

    Clean captcha widget API
     * Use explicit arguments and attributes.

--- captcha/fields.py
+++ captcha/fields.py
@@ -1,3 +1,5 @@
+import warnings
+
 from captcha.conf import settings
 from captcha.models import CaptchaStore
 import django
@@ -68,18 +70,24 @@ class BaseCaptchaTextInput(MultiWidget):
 
 
 class CaptchaTextInput(BaseCaptchaTextInput):
-    def __init__(self, attrs=None, **kwargs):
-        self._args = kwargs
-        self._args['output_format'] = self._args.get('output_format') or settings.CAPTCHA_OUTPUT_FORMAT
-        self._args['field_template'] = self._args.get('field_template') or settings.CAPTCHA_FIELD_TEMPLATE
-        # self._args['id_prefix'] = self._args.get('id_prefix')
+    def __init__(self, attrs=None, field_template=None, id_prefix=None, generator=None, output_format=None):
+        self.id_prefix = id_prefix
+        self.generator = generator
+        self.field_template = field_template or settings.CAPTCHA_FIELD_TEMPLATE
+        self.output_format = output_format or settings.CAPTCHA_OUTPUT_FORMAT
+        if self.output_format:
+            msg = ("CAPTCHA_OUTPUT_FORMAT setting and CaptchaTextInput's output_format argument are deprecated in "
+                   "favor of CAPTCHA_FIELD_TEMPLATE and field_template, respectively.")
+            warnings.warn(msg, DeprecationWarning)
 
-        if self._args['output_format'] is None and self._args['field_template'] is None:
-            raise ImproperlyConfigured('You MUST define either CAPTCHA_FIELD_TEMPLATE or CAPTCHA_OUTPUT_FORMAT setting. Please refer to http://readthedocs.org/docs/django-simple-captcha/en/latest/usage.html#installation')
+        if self.output_format is None and self.field_template is None:
+            raise ImproperlyConfigured(
+                'You MUST define CAPTCHA_FIELD_TEMPLATE setting. Please refer to '
+                'http://readthedocs.org/docs/django-simple-captcha/en/latest/usage.html#installation')
 
-        if self._args['output_format']:
+        if self.output_format:
             for key in ('image', 'hidden_field', 'text_field'):
-                if '%%(%s)s' % key not in self._args['output_format']:
+                if '%%(%s)s' % key not in self.output_format:
                     raise ImproperlyConfigured('All of %s must be present in your CAPTCHA_OUTPUT_FORMAT setting. Could not find %s' % (
                         ', '.join(['%%(%s)s' % k for k in ('image', 'hidden_field', 'text_field')]),
                         '%%(%s)s' % key
@@ -89,27 +97,27 @@ class CaptchaTextInput(BaseCaptchaTextInput):
 
     def build_attrs(self, *args, **kwargs):
         ret = super(CaptchaTextInput, self).build_attrs(*args, **kwargs)
-        if self._args.get('id_prefix') and 'id' in ret:
-            ret['id'] = '%s_%s' % (self._args.get('id_prefix'), ret['id'])
+        if self.id_prefix and 'id' in ret:
+            ret['id'] = '%s_%s' % (self.id_prefix, ret['id'])
         return ret
 
     def id_for_label(self, id_):
         ret = super(CaptchaTextInput, self).id_for_label(id_)
-        if self._args.get('id_prefix') and 'id' in ret:
-            ret = '%s_%s' % (self._args.get('id_prefix'), ret)
+        if self.id_prefix and 'id' in ret:
+            ret = '%s_%s' % (self.id_prefix, ret)
         return ret
 
     def format_output(self, rendered_widgets):
         # hidden_field, text_field = rendered_widgets
-        if self._args['output_format']:
-            ret = self._args['output_format'] % {
+        if self.output_format:
+            ret = self.output_format % {
                 'image': self.image_and_audio,
                 'hidden_field': self.hidden_field,
                 'text_field': self.text_field
             }
             return ret
 
-        elif self._args['field_template']:
+        elif self.field_template:
             context = {
                 'image': mark_safe(self.image_and_audio),
                 'hidden_field': mark_safe(self.hidden_field),
@@ -118,13 +126,13 @@ class CaptchaTextInput(BaseCaptchaTextInput):
             return render_to_string(settings.CAPTCHA_FIELD_TEMPLATE, context)
 
     def render(self, name, value, attrs=None, renderer=None):
-        self.fetch_captcha_store(name, value, attrs, self._args.get('generator'))
+        self.fetch_captcha_store(name, value, attrs, self.generator)
 
         context = {
             'image': self.image_url(),
             'name': name,
             'key': self._key,
-            'id': u'%s_%s' % (self._args.get('id_prefix'), attrs.get('id')) if self._args.get('id_prefix') else attrs.get('id')
+            'id': u'%s_%s' % (self.id_prefix, attrs.get('id')) if self.id_prefix else attrs.get('id')
         }
         if settings.CAPTCHA_FLITE_PATH:
             context.update({'audio': self.audio_url()})
