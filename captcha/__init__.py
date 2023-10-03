9afa5d4c72d10222cf2a452220b24d3a43565ab7
Marco Bonetti <marco@cruncher.ch>
2023-10-03 15:23:18 +0200

    v0.5.20 still supports Django 3.2. Fixes #222

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 5, 19)
+VERSION = (0, 5, 20)
 
 
 def get_version():
--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ copyright = "2011-2021 Marco Bonetti and contributors"
 # built documents.
 #
 # The short X.Y version.
-version = "0.5.19"
+version = "0.5.20"
 # The full version, including alpha/beta/rc tags.
 release = version
 
