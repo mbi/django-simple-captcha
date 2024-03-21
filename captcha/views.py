505559f27b31da4c854a24910af65e562c978dc5
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-03-21 17:10:35 -0300

    adds type hints for views.py

--- captcha/views.py
+++ captcha/views.py
@@ -3,13 +3,15 @@ import os
 import random
 import subprocess
 import tempfile
-from io import BytesIO
+from io import BufferedRandom, BytesIO
 
 from PIL import Image, ImageDraw, ImageFont
+from PIL.Image import Image as ImageType
+from PIL.ImageDraw import ImageDraw as ImageDrawType
 from ranged_response import RangedFileResponse
 
 from django.core.exceptions import ImproperlyConfigured
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 
 from captcha.conf import settings
 from captcha.helpers import captcha_audio_url, captcha_image_url
@@ -20,7 +22,7 @@ from captcha.models import CaptchaStore
 DISTANCE_FROM_TOP = 4
 
 
-def getsize(font, text):
+def getsize(font, text: str) -> tuple[int, int]:
     if hasattr(font, "getbbox"):
         _top, _left, _right, _bottom = font.getbbox(text)
         return _right - _left, _bottom - _top
@@ -30,31 +32,31 @@ def getsize(font, text):
         return font.getsize(text)
 
 
-def makeimg(size):
+def makeimg(size: tuple[int, int]) -> ImageType:
     if settings.CAPTCHA_BACKGROUND_COLOR == "transparent":
-        image = Image.new("RGBA", size)
+        image: ImageType = Image.new("RGBA", size)
     else:
-        image = Image.new("RGB", size, settings.CAPTCHA_BACKGROUND_COLOR)
+        image: ImageType = Image.new("RGB", size, settings.CAPTCHA_BACKGROUND_COLOR)
     return image
 
 
-def captcha_image(request, key, scale=1):
+def captcha_image(request: HttpRequest, key, scale: int=1) -> HttpResponse:
     if scale == 2 and not settings.CAPTCHA_2X_IMAGE:
         raise Http404
     try:
-        store = CaptchaStore.objects.get(hashkey=key)
+        store: CaptchaStore = CaptchaStore.objects.get(hashkey=key)
     except CaptchaStore.DoesNotExist:
         # HTTP 410 Gone status so that crawlers don't index these expired urls.
         return HttpResponse(status=410)
 
     random.seed(key)  # Do not generate different images for the same key
 
-    text = store.challenge
+    text: str = store.challenge
 
     if isinstance(settings.CAPTCHA_FONT_PATH, str):
-        fontpath = settings.CAPTCHA_FONT_PATH
+        fontpath: str = settings.CAPTCHA_FONT_PATH
     elif isinstance(settings.CAPTCHA_FONT_PATH, (list, tuple)):
-        fontpath = random.choice(settings.CAPTCHA_FONT_PATH)
+        fontpath: str = random.choice(settings.CAPTCHA_FONT_PATH)
     else:
         raise ImproperlyConfigured(
             "settings.CAPTCHA_FONT_PATH needs to be a path to a font or list of paths to fonts"
@@ -71,28 +73,32 @@ def captcha_image(request, key, scale=1):
         size = getsize(font, text)
         size = (size[0] * 2, int(size[1] * 1.4))
 
-    image = makeimg(size)
+    image: ImageType = makeimg(size)
     xpos = 2
 
-    charlist = []
+    charlist: list[str] = []
+
     for char in text:
         if char in settings.CAPTCHA_PUNCTUATION and len(charlist) >= 1:
             charlist[-1] += char
         else:
             charlist.append(char)
+
     for char in charlist:
-        fgimage = Image.new("RGB", size, settings.CAPTCHA_FOREGROUND_COLOR)
-        charimage = Image.new("L", getsize(font, " %s " % char), "#000000")
-        chardraw = ImageDraw.Draw(charimage)
+        fgimage: ImageType = Image.new("RGB", size, settings.CAPTCHA_FOREGROUND_COLOR)
+        charimage: ImageType = Image.new("L", getsize(font, " %s " % char), "#000000")
+        chardraw: ImageDrawType = ImageDraw.Draw(charimage)
         chardraw.text((0, 0), " %s " % char, font=font, fill="#ffffff")
+
         if settings.CAPTCHA_LETTER_ROTATION:
-            charimage = charimage.rotate(
+            charimage: ImageType = charimage.rotate(
                 random.randrange(*settings.CAPTCHA_LETTER_ROTATION),
-                expand=0,
+                expand=False,
                 resample=Image.BICUBIC,
             )
-        charimage = charimage.crop(charimage.getbbox())
-        maskimage = Image.new("L", size)
+
+        charimage: ImageType = charimage.crop(charimage.getbbox())
+        maskimage: ImageType = Image.new("L", size)
 
         maskimage.paste(
             charimage,
@@ -103,13 +109,14 @@ def captcha_image(request, key, scale=1):
                 DISTANCE_FROM_TOP + charimage.size[1],
             ),
         )
-        size = maskimage.size
-        image = Image.composite(fgimage, image, maskimage)
-        xpos = xpos + 2 + charimage.size[0]
+
+        size: tuple[int, int] = maskimage.size
+        image: ImageType = Image.composite(fgimage, image, maskimage)
+        xpos: int = xpos + 2 + charimage.size[0]
 
     if settings.CAPTCHA_IMAGE_SIZE:
         # centering captcha on the image
-        tmpimg = makeimg(size)
+        tmpimg: ImageType = makeimg(size)
         tmpimg.paste(
             image,
             (
@@ -117,17 +124,18 @@ def captcha_image(request, key, scale=1):
                 int((size[1] - charimage.size[1]) / 2 - DISTANCE_FROM_TOP),
             ),
         )
-        image = tmpimg.crop((0, 0, size[0], size[1]))
+        image: ImageType = tmpimg.crop((0, 0, size[0], size[1]))
     else:
-        image = image.crop((0, 0, xpos + 1, size[1]))
-    draw = ImageDraw.Draw(image)
+        image: ImageType = image.crop((0, 0, xpos + 1, size[1]))
+
+    draw: ImageDrawType = ImageDraw.Draw(image)
 
     for f in settings.noise_functions():
         draw = f(draw, image)
     for f in settings.filter_functions():
         image = f(image)
 
-    out = BytesIO()
+    out: BytesIO = BytesIO()
     image.save(out, "PNG")
     out.seek(0)
 
@@ -145,28 +153,30 @@ def captcha_image(request, key, scale=1):
     return response
 
 
-def captcha_audio(request, key):
+def captcha_audio(request: HttpRequest, key) -> HttpResponse | RangedFileResponse:
     if settings.CAPTCHA_FLITE_PATH:
         try:
-            store = CaptchaStore.objects.get(hashkey=key)
+            store: CaptchaStore = CaptchaStore.objects.get(hashkey=key)
         except CaptchaStore.DoesNotExist:
             # HTTP 410 Gone status so that crawlers don't index these expired urls.
             return HttpResponse(status=410)
 
-        text = store.challenge
+        text: str = store.challenge
+
         if "captcha.helpers.math_challenge" == settings.CAPTCHA_CHALLENGE_FUNCT:
             text = text.replace("*", "times").replace("-", "minus").replace("+", "plus")
         else:
             text = ", ".join(list(text))
-        path = str(os.path.join(tempfile.gettempdir(), "%s.wav" % key))
+
+        path: str = str(os.path.join(tempfile.gettempdir(), "%s.wav" % key))
         subprocess.call([settings.CAPTCHA_FLITE_PATH, "-t", text, "-o", path])
 
         # Add arbitrary noise if sox is installed
         if settings.CAPTCHA_SOX_PATH:
-            arbnoisepath = str(
+            arbnoisepath: str = str(
                 os.path.join(tempfile.gettempdir(), "%s_arbitrary.wav") % key
             )
-            mergedpath = str(os.path.join(tempfile.gettempdir(), "%s_merged.wav") % key)
+            mergedpath: str = str(os.path.join(tempfile.gettempdir(), "%s_merged.wav") % key)
             subprocess.call(
                 [
                     settings.CAPTCHA_SOX_PATH,
@@ -200,13 +210,15 @@ def captcha_audio(request, key):
 
         if os.path.isfile(path):
             # Move the response file to a filelike that will be deleted on close
-            temporary_file = tempfile.TemporaryFile()
+            temporary_file: BufferedRandom = tempfile.TemporaryFile()
+
             with open(path, "rb") as original_file:
                 temporary_file.write(original_file.read())
+
             temporary_file.seek(0)
             os.remove(path)
 
-            response = RangedFileResponse(
+            response: RangedFileResponse = RangedFileResponse(
                 request, temporary_file, content_type="audio/wav"
             )
             response["Content-Disposition"] = 'attachment; filename="{}.wav"'.format(
@@ -216,13 +228,13 @@ def captcha_audio(request, key):
     raise Http404
 
 
-def captcha_refresh(request):
+def captcha_refresh(request: HttpRequest) -> HttpResponse:
     """Return json with new captcha for ajax refresh request"""
     if not request.headers.get("x-requested-with") == "XMLHttpRequest":
         raise Http404
 
-    new_key = CaptchaStore.pick()
-    to_json_response = {
+    new_key: str = CaptchaStore.pick()
+    to_json_response: dict[str, str | None] = {
         "key": new_key,
         "image_url": captcha_image_url(new_key),
         "audio_url": captcha_audio_url(new_key)
