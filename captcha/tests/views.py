c92fdeedba38d526e4f51de5be6b33fc64639bb8
Marco Bonetti <marco@cruncher.ch>
2024-11-15 08:55:46 +0100

    chore: switch the test template to html5

--- captcha/tests/views.py
+++ captcha/tests/views.py
@@ -12,8 +12,7 @@ from captcha.serializers import CaptchaModelSerializer, CaptchaSerializer
 
 
 TEST_TEMPLATE = r"""
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!doctype html>
 <html>
     <head>
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
